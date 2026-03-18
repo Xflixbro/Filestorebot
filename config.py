f13c809646c2626338d39a7b46358bcc154f234b
Xflixbro <yedhuku2004@gmail.com>
2026-03-18 12:21:17 +0530

    Update config.py
    Ii

--- config.py
+++ config.py
@@ -18,18 +18,18 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339:")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", "27050683")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "013a5c0b1f2c320b98236cf212835d59") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003831800241")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "sewxiy") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001")) # Owner id
+OWNER = os.environ.get("OWNER", "amalvj007") # Owner username without @
+OWNER_ID = int(os.environ.get("OWNER_ID", "6048003536")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8000")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://RENDERTEST:RENDERTEST@cluster0.5mowi1z.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "CORNXVILLA")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
