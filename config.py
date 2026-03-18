8dc568e86e2955146b415cb7045aea4b4a502629
Xflixbro <yedhuku2004@gmail.com>
2026-03-18 12:37:34 +0530

    Update config.py
    Gxtx

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ APP_ID = int(os.environ.get("APP_ID", "27050683")) #Your API ID from my.telegram
 API_HASH = os.environ.get("API_HASH", "013a5c0b1f2c320b98236cf212835d59") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003831800241")) #Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002162795137")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "amalvj007") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "6048003536")) # Owner id
 #--------------------------------------------
