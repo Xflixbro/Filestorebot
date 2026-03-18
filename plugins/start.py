e9df1cf6a030bea85727cdd66bccba09160a2b03
Xflixbro <yedhuku2004@gmail.com>
2026-03-18 14:02:53 +0530

    Update start.py
    Jk

--- plugins/start.py
+++ plugins/start.py
@@ -93,7 +93,7 @@ async def start_command(client: Client, message: Message):
                 await db.update_verify_status(id, verify_token=token, link="")
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://telegram.dog/{client.username}?start=verify_{token}')
                 btn = [
-                    [InlineKeyboardButton("• ᴏᴘᴇɴ ʟɪɴᴋ •", url=link),
+                    [InlineKeyboardButton("• ᴠᴇʀɪꜰʏ •", url=link),
                      InlineKeyboardButton("• ᴛᴜᴛᴏʀɪᴀʟ •", url=TUT_VID)],
                     [InlineKeyboardButton("• ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ •", callback_data="premium")]
                 ]
